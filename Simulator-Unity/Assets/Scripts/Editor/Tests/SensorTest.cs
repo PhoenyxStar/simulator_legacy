4cd3e8ea6ce15d83941d905f79a23b65df9f72aa
Hengstar <gongxinheng@gmail.com>
2015-11-08 23:56:06 -0800

    1. Modified Sensors.cs, added public properties for independent testing 2. Added Sensor Test script by using NUnit framework 3. Created static(editor) testing folder

---
+++ Simulator-Unity/Assets/Scripts/Editor/Tests/SensorTest.cs
@@ -0,0 +1,94 @@
+﻿// Team Robosub
+using UnityEngine;
+using NUnit.Framework;
+using System;
+
+namespace TeamStark.RobosubSimulator
+{
+    [TestFixture]
+    [Category("Sensor Tests")]
+    public class SensorTest
+    {
+        private Sensors sensor;
+
+        public SensorTest()
+        {
+            sensor = new Sensors();
+        }
+
+        /*
+            // Depth test
+            for (int i = 0; i < 15; i++)
+            {
+                sensor.Depth = i;
+                double d = ((sensor.Depth * 3.28084) + 10.341) / .1728;
+                if (Math.Abs(sensor.GetDepthOutput() - d) >= 0.0001)
+                {
+                    Debug.LogError("output error. depth not equal");
+                    return false;
+                }
+            }
+        */
+        [Test]
+        [Category("Depth Test")]
+        public void TestDepth([NUnit.Framework.Range(0, 15, 1)] int d)
+        {
+            double error = 0.0001;
+            sensor.Depth = d;
+            double depth = ((sensor.Depth * 3.28084) + 10.341) / .1728;
+            Assert.Less(Math.Abs(sensor.GetDepthOutput() - depth), error, depth.ToString() + "!=" + sensor.GetDepthOutput().ToString());
+        }
+
+        /*
+            // GetGyroscopeOutput test
+            for (float i = -5.0f; i < 5.0f; i += 0.1f)
+            {
+                sensor.AngularVelocity = new Vector3(i, i, i);
+                Vector3 v = sensor.AngularVelocity / 0.0174533f * 14.375f;
+                if (Math.Abs(sensor.GetGyroscopeOutput().x - v.x) >= 0.1f ||
+                    Math.Abs(sensor.GetGyroscopeOutput().y - v.y) >= 0.1f ||
+                    Math.Abs(sensor.GetGyroscopeOutput().z - v.z) >= 0.1f)
+                {
+                    Debug.LogError("output error. angularVelocity not equal");
+                    return false;
+                }
+            }
+        */
+        [Test]
+        [Category("Gyroscope Test")]
+        public void TestGyroscope([NUnit.Framework.Range(-5.0f, 5.0f, 0.1f)] float av)
+        {
+            float error = 0.1f;
+            sensor.AngularVelocity = new Vector3(av, av, av);
+            Vector3 v = sensor.AngularVelocity / 0.0174533f * 14.375f;
+            Assert.Less(Math.Abs(sensor.GetGyroscopeOutput().x - v.x), error);
+            Assert.Less(Math.Abs(sensor.GetGyroscopeOutput().y - v.y), error);
+            Assert.Less(Math.Abs(sensor.GetGyroscopeOutput().z - v.z), error);
+        }
+
+        /*
+            for (float i = -5.0f; i < 5.0f; i += 0.1f)
+            {
+                Vector3 v = new Vector3(i, i, i);
+                sensor.Acceleration = v;
+                if (v != sensor.GetAccelerometerOutput() / (256.0f / 9.81f))
+                {
+                    Debug.LogError("output error. GetAccelerometerOutput not equal");
+                    return false;
+                }
+            }
+         */
+        [Test]
+        [Category("Accelerometer Test")]
+        public void TestAccelerometer([NUnit.Framework.Range(-5.0f, 5.0f, 0.1f)] float am)
+        {
+            float error = 0.0001f;
+            Vector3 newam = new Vector3(am, am, am);
+            sensor.Acceleration = newam;
+            //Assert.AreEqual(newam, sensor.GetAccelerometerOutput() / (256.0f / 9.81f));
+            Assert.Less(Math.Abs(sensor.GetAccelerometerOutput().x / (256.0f / 9.81f) - newam.x), error);
+            Assert.Less(Math.Abs(sensor.GetAccelerometerOutput().y / (256.0f / 9.81f) - newam.y), error);
+            Assert.Less(Math.Abs(sensor.GetAccelerometerOutput().z / (256.0f / 9.81f) - newam.z), error);
+        }
+    }
+}
--- Simulator-Unity/Assets/Scripts/Sensors.cs
+++ Simulator-Unity/Assets/Scripts/Sensors.cs
@@ -1,136 +1,178 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Sensors : MonoBehaviour {
-
-    private GameObject Sub = GameObject.Find("Sub");
-
-    private Vector3 velocity;
-    private Vector3 lastVelocity;
-    private double depth;
-    private Vector3 acceleration;
-    private Vector3 magneticField;
-    private Vector3 angularVelocity;
-
-	// Use this for initialization
-	void Start ()
+namespace TeamStark.RobosubSimulator
+{
+    public class Sensors : MonoBehaviour
     {
-        velocity = new Vector3(0, 0, 0);
-        lastVelocity = new Vector3(0, 0, 0);
-        depth = 0;
-        acceleration = new Vector3(0, 0, 0);
-        magneticField = new Vector3(0, 0, 0);
-        angularVelocity = new Vector3(0, 0, 0);
+
+        private GameObject Sub = GameObject.Find("Sub");
+
+        private Vector3 velocity;
+        private Vector3 lastVelocity;
+        private double depth;
+        private Vector3 acceleration;
+        private Vector3 magneticField;
+        private Vector3 angularVelocity;
+
+        public double Depth
+        {
+            get
+            {
+                return depth;
+            }
+            set
+            {
+                depth = value;
+            }
+        }
+
+        public Vector3 AngularVelocity
+        {
+            get
+            {
+                return angularVelocity;
+            }
+            set
+            {
+                angularVelocity = value;
+            }
+        }
+
+        public Vector3 Acceleration
+        {
+            get
+            {
+                return acceleration;
+            }
+            set
+            {
+                acceleration = value;
+            }
+        }
+
+
+        // Use this for initialization
+        void Start()
+        {
+            velocity = new Vector3(0, 0, 0);
+            lastVelocity = new Vector3(0, 0, 0);
+            depth = 0;
+            acceleration = new Vector3(0, 0, 0);
+            magneticField = new Vector3(0, 0, 0);
+            angularVelocity = new Vector3(0, 0, 0);
+        }
+
+        // Update is called once per frame
+        void LastUpdate()
+        {
+            UpdateDepth();
+            UpdateVelocity();
+            UpdateAcceleration();
+            UpdateMagneticField();
+            UpdateAngularVelocity();
+        }
+
+        void UpdateDepth()
+        {
+            GameObject water = GameObject.Find("WaterBasicDaytime");
+
+            // TODO: Figure out how Unity water works. Is this correct for its top? 
+            double waterTop = ((Transform)water.GetComponent("Transform")).position.y;
+            double subCenter = ((Transform)Sub.GetComponent("Transform")).position.y;
+
+            depth = waterTop - subCenter;
+        }
+
+        void UpdateVelocity()
+        {
+            Rigidbody rb = (Rigidbody)Sub.GetComponent("Rigidbody");
+            lastVelocity = velocity;
+            velocity = rb.velocity;
+        }
+
+        /// <summary>
+        /// Updates acceleration. Must be called after UpdateVelocity.
+        /// </summary>
+        void UpdateAcceleration()
+        {
+            // TODO: Add gravity
+            acceleration = velocity - lastVelocity;
+        }
+
+        void UpdateMagneticField()
+        {
+            // TODO
+        }
+
+        void UpdateAngularVelocity()
+        {
+            Rigidbody rb = (Rigidbody)Sub.GetComponent("Rigidbody");
+            angularVelocity = rb.angularVelocity;
+        }
+
+        /// <summary>
+        /// Measures the submarine's depth.
+        /// depth_feet = (sensor_value * 0.1728) - 10.341
+        /// </summary>
+        /// <returns></returns>
+        public double GetDepthOutput()
+        {
+            // 1 Unity unit = 1 meter
+            double depthInFeet = depth * 3.280839895;
+
+            return (depthInFeet + 10.341) / 0.1728;
+        }
+
+        /// <summary>
+        /// Measures acceleration in x,y,z direction. 
+        /// The units are such that a value of ~256 on the sensor is equal to the force of gravity (1 g, or 9.81 m/s2)
+        /// </summary>
+        /// <returns></returns>
+        public Vector3 GetAccelerometerOutput()
+        {
+            var output = (acceleration * (float)(256 / 9.81));
+
+            return output;
+        }
+
+        /// <summary>
+        /// Measures angular velocity in the X, Y, and Z axes.
+        /// The units are such that a value of ~14.375 on the sensor is equal to a rotation speed of 1 degree/second.
+        /// </summary>
+        /// <returns></returns>
+        public Vector3 GetGyroscopeOutput()
+        {
+            // Convert radians/second to degrees/second (* 57.2958),
+            // then adjust to sensor specs (* 14.375)
+            return angularVelocity * (float)57.2958 * (float)14.375;
+        }
+
+        /// <summary>
+        /// Measures magnetic field strength in the X, Y, and Z axes.
+        /// This will not just give us an angle of degrees from north, post-processing is needed.
+        /// The units are such that a value of ~10 on the sensor is equal to 1 µT
+        /// </summary>
+        /// <returns></returns>
+        public Vector3 GetMagnetometerOutput()
+        {
+            // TODO (Currently unimplemented in real AUV)
+            return magneticField;
+        }
+
+        /// <summary>
+        /// Always shows full battery within simulator.
+        /// actual_voltage = sensor_value * (3.3/1024*7.5)
+        /// </summary>
+        /// <returns></returns>
+        public double GetBatteryOutput()
+        {
+            // TODO: Check sub's actual voltage
+            // Assuming 24 V
+            double voltage = 24;
+            return voltage / (3.3 / (double)1024 * 7.5);
+        }
+
+        // TODO: Mission Start Switch
     }
-	
-	// Update is called once per frame
-	void LastUpdate () {
-        UpdateDepth();
-        UpdateVelocity();
-        UpdateAcceleration();
-        UpdateMagneticField();
-        UpdateAngularVelocity();
-	}
-
-    void UpdateDepth()
-    {
-        GameObject water = GameObject.Find("WaterBasicDaytime");
-
-        // TODO: Figure out how Unity water works. Is this correct for its top? 
-        double waterTop = ((Transform)water.GetComponent("Transform")).position.y;
-        double subCenter = ((Transform)Sub.GetComponent("Transform")).position.y;
-
-        depth = waterTop - subCenter;
-    }
-
-    void UpdateVelocity()
-    {
-        Rigidbody rb = (Rigidbody)Sub.GetComponent("Rigidbody");
-        lastVelocity = velocity;
-        velocity = rb.velocity;
-    }
-
-    /// <summary>
-    /// Updates acceleration. Must be called after UpdateVelocity.
-    /// </summary>
-    void UpdateAcceleration()
-    {
-        // TODO: Add gravity
-        acceleration = velocity - lastVelocity;
-    }
-
-    void UpdateMagneticField()
-    {
-        // TODO
-    }
-
-    void UpdateAngularVelocity()
-    {
-        Rigidbody rb = (Rigidbody)Sub.GetComponent("Rigidbody");
-        angularVelocity = rb.angularVelocity;
-    }
-
-    /// <summary>
-    /// Measures the submarine's depth.
-    /// depth_feet = (sensor_value * 0.1728) - 10.341
-    /// </summary>
-    /// <returns></returns>
-    public double GetDepthOutput()
-    {
-        // 1 Unity unit = 1 meter
-        double depthInFeet = depth * 3.280839895;
-
-        return (depthInFeet + 10.341) / 0.1728;
-    }
-
-    /// <summary>
-    /// Measures acceleration in x,y,z direction. 
-    /// The units are such that a value of ~256 on the sensor is equal to the force of gravity (1 g, or 9.81 m/s2)
-    /// </summary>
-    /// <returns></returns>
-    public Vector3 GetAccelerometerOutput()
-    {
-        var output = (acceleration * (float)(256/9.81));
-
-        return output;
-    }
-
-    /// <summary>
-    /// Measures angular velocity in the X, Y, and Z axes.
-    /// The units are such that a value of ~14.375 on the sensor is equal to a rotation speed of 1 degree/second.
-    /// </summary>
-    /// <returns></returns>
-    public Vector3 GetGyroscopeOutput()
-    {
-        // Convert radians/second to degrees/second (* 57.2958),
-        // then adjust to sensor specs (* 14.375)
-        return angularVelocity * (float)57.2958 * (float)14.375;
-    }
-
-    /// <summary>
-    /// Measures magnetic field strength in the X, Y, and Z axes.
-    /// This will not just give us an angle of degrees from north, post-processing is needed.
-    /// The units are such that a value of ~10 on the sensor is equal to 1 µT
-    /// </summary>
-    /// <returns></returns>
-    public Vector3 GetMagnetometerOutput()
-    {
-        // TODO (Currently unimplemented in real AUV)
-        return magneticField;
-    }
-
-    /// <summary>
-    /// Always shows full battery within simulator.
-    /// actual_voltage = sensor_value * (3.3/1024*7.5)
-    /// </summary>
-    /// <returns></returns>
-    public double GetBatteryOutput()
-    {
-        // TODO: Check sub's actual voltage
-        // Assuming 24 V
-        double voltage = 24;
-        return voltage / (3.3 / (double)1024 * 7.5);
-    }
-
-    // TODO: Mission Start Switch
 }
