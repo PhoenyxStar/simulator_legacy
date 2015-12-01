503abd4d427d6e2b1891a54cbaec7b8c2466df3d
Hengstar <xgong@eecs.wsu.edu>
2015-12-01 00:17:04 -0800

    Replaced original water with better water effect and added UnderWater effect script (not used for now)

---
+++ Simulator-Unity/Assets/Scripts/UnderWater.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnderWater : MonoBehaviour
+{
+
+    //This script enables underwater effects. Attach to main camera.
+
+    //Define variable
+    public float underwaterLevel = 16;
+
+    //The scene's default fog settings
+    private bool defaultFog;
+    private Color defaultFogColor;
+    private float defaultFogDensity;
+    private Material defaultSkybox;
+    private Material noSkybox;
+
+    void Start()
+    {
+        defaultFog = RenderSettings.fog;
+        defaultFogColor = RenderSettings.fogColor;
+        defaultFogDensity = RenderSettings.fogDensity;
+        Material defaultSkybox = RenderSettings.skybox;
+        Material noSkybox = RenderSettings.skybox;
+        //Set the background color
+        GetComponent<Camera>().backgroundColor = new Color(0, 0.4f, 0.7f, 1);
+    }
+
+    void Update()
+    {
+        if (transform.position.y < underwaterLevel)
+        {
+            RenderSettings.fog = true;
+            RenderSettings.fogColor = new Color(0, 0.4f, 0.7f, 0.6f);
+            RenderSettings.fogDensity = 0.04f;
+            RenderSettings.skybox = noSkybox;
+        }
+        else
+        {
+            RenderSettings.fog = defaultFog;
+            RenderSettings.fogColor = defaultFogColor;
+            RenderSettings.fogDensity = defaultFogDensity;
+            RenderSettings.skybox = defaultSkybox;
+        }
+    }
+}
